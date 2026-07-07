61a3f41f8fcdf75177cbd5d40f0fd4e487f91c11
Marco Thulio Martins Cacique <65259289+MarcoThulio1207@users.noreply.github.com>
2026-07-07 14:22:04 -0300

    Clear database connection
    Updated database connection parameters to empty strings.

--- api/db.js
+++ api/db.js
@@ -4,6 +4,6 @@ import mysql from 'mysql';
 export const db = mysql.createConnection({
     host: 'localhost',
     user:'root',
-    password:'C031@N#7z<Xk',
-    database:'crud'
+    password:'',
+    database:''
 });
